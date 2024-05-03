7171e87585910c9b1049833ff6f5bac4b92808a6
Ivan Zyulin <solar.ivz@gmail.com>
2024-05-03 22:02:30 +1200

    add Fighter class

--- main.py
+++ main.py
@@ -15,3 +15,20 @@ class Bow(Weapon):
     def attack(self):
         print("Боец наносит удар из лука.")
 
+# класс Fighter
+
+class Fighter:
+    def __init__(self, name):
+        self.name = name
+        self.weapon = None
+
+    def change_weapon(self, weapon):
+        self.weapon = weapon
+
+    def attack(self):
+        if self.weapon:
+            print(f"{self.name} выбирает {self.weapon.__class__.__name__}.")
+            self.weapon.attack()
+        else:
+            print("Боец не выбрал оружие.")
+
